5623c588d8ae84afe9b485357de69a663a72e4b6
n1como <n1como@163.com>
2020-08-11 18:42:12 +0800

    update npm & node

--- front/config/index.js
+++ front/config/index.js
@@ -10,17 +10,7 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {
-      '/api': {     //这里最好有一个 /
-        target: 'http://127.0.0.1:8089',  // 后台接口域名
-        ws: true,        //如果要代理 websockets，配置这个参数
-        secure: false,  // 如果是https接口，需要配置这个参数
-        changeOrigin: true,  //是否跨域
-        pathRewrite:{
-            '^/api':''
-        }
-    }
-    },
+    proxyTable: {},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
