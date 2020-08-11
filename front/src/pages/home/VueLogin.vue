9fde42720758f67030a022539ad0f2d33b0c9a83
n1como <n1como@163.com>
2020-08-11 15:05:09 +0800

    using element-ui implement login

---
+++ front/src/pages/home/VueLogin.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+<el-input placeholder="请输入内容" v-model="input" clearable></el-input>
+<br />
+<el-input placeholder="请输入密码" v-model="input" show-password></el-input>
+<el-row>
+  <el-button plain>清除</el-button>
+  <el-button plain>登录</el-button>
+</el-row>
+</div>
+</template>
+
+<style>
+  .el-input {
+    width:200px;
+  }
+  
+  .el-aside {
+    color: #333;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      };
+      return {
+        input: '',
+        tableData: Array(20).fill(item)
+      }
+    }
+  };
+</script>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import Login from '@/pages/home/Login'
+import VueLogin from '@/pages/home/VueLogin'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/vuelogin',
+      name: 'VueLogin',
+      component: VueLogin
     }
   ]
 })
