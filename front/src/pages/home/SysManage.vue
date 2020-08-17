c2cbcd4dd8fd09c215332b8cf072c6c403c871c2
whale4u <whale4u@hotmail.com>
2020-08-18 00:40:01 +0800

    增加对话框

--- front/src/pages/home/SysManage.vue
+++ front/src/pages/home/SysManage.vue
@@ -1,37 +1,139 @@
 <template>
-    <el-table
-      :data="tableData"
-      style="width: 100%">
-      <el-table-column
-        prop="SysChineseName"
-        label="系统中文名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="SysEnglishName"
-        label="系统英文名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="SysAddress"
-        label="URL地址">
-      </el-table-column>
-    </el-table>
-  </template>
+<div>
+  <el-card class="box-card">
+  <div style="float: left;padding: 5px 5px;">
+    <el-button type="primary" @click="dialogFormVisible = true">新建</el-button>
+    <el-button type="primary" @click="1">删除</el-button>
+    <el-button type="primary" @click="1">修改</el-button>
+  </div>
+  <el-table
+    ref="multipleTable"
+    :data="tableData3"
+    tooltip-effect="dark"
+    style="width: 100%"
+    @selection-change="handleSelectionChange">
+    <el-table-column
+      type="selection"
+      width="55">
+    </el-table-column>
+    <el-table-column
+      label="日期"
+      width="120">
+      <template slot-scope="scope">{{ scope.row.date }}</template>
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址"
+      show-overflow-tooltip>
+    </el-table-column>
+  </el-table>
+  <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+  <el-form :model="form">
+    <el-form-item label="活动名称" :label-width="formLabelWidth">
+      <el-input v-model="form.name" auto-complete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="活动区域" :label-width="formLabelWidth">
+      <el-select v-model="form.region" placeholder="请选择活动区域">
+        <el-option label="区域一" value="shanghai"></el-option>
+        <el-option label="区域二" value="beijing"></el-option>
+      </el-select>
+    </el-form-item>
+  </el-form>
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="dialogFormVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+  </div>
+  </el-dialog>
+  </el-card>
+</div>
+</template>
 
-  <script>
-    export default {
-      name: 'SysManage',
-      data() {
-        return {
-          tableData: [{
-            SysChineseName: '百度',
-            SysEnglishName: 'Baidu',
-            SysAddress: 'www.baidu.com'
-          }, ]
+<script>
+  export default {
+    data() {
+      return {
+        dialogTableVisible: false,
+        dialogFormVisible: false,
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '120px',
+        tableData3: [{
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-08',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-06',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-07',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        multipleSelection: []
+      }
+    },
+
+    methods: {
+      toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
         }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
       }
     }
-  </script>
+  }
+</script>
 
-  <style></style>
+  <style>
+.el-card__body {
+  padding: 0;
+}
+.el-main {
+    display: block;
+    -webkit-box-flex: 1;
+    -ms-flex: 1;
+    flex: 1;
+    -ms-flex-preferred-size: auto;
+    flex-basis: auto;
+    overflow: auto;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    padding: 0;
+}
+  </style>
