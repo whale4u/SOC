37e68ca64acba686da0ed7f998ad9b9ed335ec98
whale4u <whale4u@hotmail.com>
2020-08-18 00:01:20 +0800

    删除mockjs

--- front/src/pages/home/Home.vue
+++ front/src/pages/home/Home.vue
@@ -1,7 +1,5 @@
 <template>
     <div>Home
-    <div v-model="data"></div>
-    <button @click="submitForm">submit</button>
     </div>
 </template>
 
@@ -14,19 +12,8 @@ export default {
             data: 0
         }
     },
-    methods: {
-        submitForm () {
-            axios.get('/api/hello',{
-                headers: {
-                    'authorization': 'Bearer ' + sessionStorage.getItem('token')
-                },
-            })
-            .then(function (response) {
-                console.log(response)
-            })
-        }
-    },
     mounted() {
+        console.log(11111)
         axios.get('/user/userinfo')
         .then(res => {
             console.log(res)
